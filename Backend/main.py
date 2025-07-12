ded321e3369a507e915ee6e7b1e60798ff5be860
Senyo-SA <sadzivor@gmail.com>
2025-07-12 11:57:13 +0100

    more setup and calibrations

--- Backend/main.py
+++ Backend/main.py
@@ -8,5 +8,5 @@ load_dotenv()
 # llm1 = ChatOpenAI(model="gpt-4o-mini")
 llm2 = ChatAnthropic(model="claude-opus-4-20250514")
 
-result = llm2.invoke("what is the meaning of AI")
-print(result)
+result = llm2.invoke("random computing fact")
+print(result.content)
